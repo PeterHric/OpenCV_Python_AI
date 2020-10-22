7fb5f10dd196d39fa7ab067d141c43e53a6e681a
Peter Hric <hric.peter@gmail.com>
2020-10-22 16:31:08 +0200

    Renaming ShapesRecognition + adding Face Recognition

---
+++ FaceRecognition_OpenCV.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+
+faceCascade = cv.CascadeClassifier("Resources/haarcascade_frontal_default.xml")
+img = cv.imread('Resources/lena_Large.jpg')
+imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+# Detect and store the faces into an object describing the rectangle around detected face
+faces = faceCascade.detectMultiScale(imgGray,
+                                     1.1,  #  Scale factor  - try tweak
+                                     4)    #  Min neughbours - try tweak
+
+for (x, y, w, h) in faces:
+    cv.rectangle(img, (x, y),(x+w, y+h), (255, 0 ,0), 2)
+
+cv.imshow("Result", img)
+cv.waitKey(0)
