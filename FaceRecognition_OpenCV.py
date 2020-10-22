8aa0e8a0a4f1fa3ed5f83906ae4e5c311f2ee4a0
Peter Hric <hric.peter@gmail.com>
2020-10-22 18:46:07 +0200

    Adding document scanning example

--- FaceRecognition_OpenCV.py
+++ FaceRecognition_OpenCV.py
@@ -1,13 +1,14 @@
 import cv2 as cv
 
 faceCascade = cv.CascadeClassifier("Resources/haarcascade_frontal_default.xml")
-img = cv.imread('Resources/lena_Large.jpg')
+# img = cv.imread('Resources/lena_Large.jpg')
+img = cv.imread('Resources/Face_1.jpeg')
 imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
 # Detect and store the faces into an object describing the rectangle around detected face
 faces = faceCascade.detectMultiScale(imgGray,
-                                     1.1,  #  Scale factor  - try tweak
-                                     4)    #  Min neughbours - try tweak
+                                     1.4,  #  Scale factor  - try tweak
+                                     1)    #  Min neighbours - try tweak
 
 for (x, y, w, h) in faces:
     cv.rectangle(img, (x, y),(x+w, y+h), (255, 0 ,0), 2)
